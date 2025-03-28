240fc0a795fc1400a770a4bd139ddb1b86e29bd7
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-03-28 06:58:37 -0700

    fix: make header sticky

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,18 +8,22 @@ const Header = () => {
 
 
   return (
-    <header className="bg-dark text-white p-3 shadow">
-        <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex align-items-center gap-2">
-            {/* <Shield size={24} /> */}
-            <img src="./eagle.png" style={{width: "20px", height: "30px"}} alt="" />
-            <h1 className="h5 mb-0 fw-bold">Eagle Eye</h1>
-            </div>
-            <div className="d-flex align-items-center gap-3">
-            <Bell size={20} />
-            <Menu size={20} />
-            </div>
-        </div>
+    <header className="bg-dark text-white p-3 shadow" style={{
+      position: "sticky",
+      top: 0,
+      zIndex: 1000,
+    }}>
+      <div className="d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center gap-2">
+          {/* <Shield size={24} /> */}
+          <img src="./eagle.png" style={{width: "20px", height: "30px"}} alt="" />
+          <h1 className="h5 mb-0 fw-bold">Eagle Eye</h1>
+          </div>
+          <div className="d-flex align-items-center gap-3">
+          <Bell size={20} />
+          <Menu size={20} />
+          </div>
+      </div>
     </header>
   )
 }
